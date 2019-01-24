53299d897a55c60f09c9af89f7580b72037c7eb4
uchiyamakoki <454768497@qq.com>
2019-01-24 14:12:17 +0800

    thinkphp初体验

---
+++ Application/Admin/Controller/LoginController.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+
+/**
+ * use Common\Model 这块可以不需要使用，框架默认会加载里面的内容
+ */
+class LoginController extends Controller {
+
+    public function index(){
+        // admin.php?c=index
+        return $this->display();
+    }
+
+    public function check(){
+        echo "check_success";
+    }
+}
